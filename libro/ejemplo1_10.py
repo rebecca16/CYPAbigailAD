25997f4b66380c9205335ea2ae22777f6df01ecf
rebecca16 <rebeccaaguilar688@gmail.com>
2019-09-17 16:51:08 -0500

    Cree ejemplo1_10

---
+++ libro/ejemplo1_10.py
@@ -0,0 +1,6 @@
+BASE = int (input ("Dame la base de tu rectangulo"))
+ALTU = int (input ("Dame la altura de tu rectangulo"))
+SUP = BASE * ALTU
+PER = 2 * (BASE + ALTU)
+print (f"La superficie de tu rectangulo es { SUP }")
+print (f"El perimetro de tu rectangulo es { PER }")
