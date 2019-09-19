c0a59dfad99fe05afd05f8a39048ae854a81056a
rebecca16 <rebeccaaguilar688@gmail.com>
2019-09-18 23:51:58 -0500

    Se creo Ejemplo1_14

---
+++ libro/Ejemplo1_14.py
@@ -0,0 +1,6 @@
+# El programa, dado como dato un numero entero positivo, calcula el cuadrado y el cubo de dicho numero
+NUM = int (input("Introduce un numero entero positivo "))
+CUA = NUM * NUM
+CUB = NUM ** 3
+print (f"El cuadrado de tu numero es { CUA }")
+print (f"El cubo de tu numero es { CUB }")
