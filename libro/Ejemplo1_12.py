6e7ad84df4798a579601248c373ff2396090231e
rebecca16 <rebeccaaguilar688@gmail.com>
2019-09-19 00:05:04 -0500

    Se creo Ejemplo1_12

---
+++ libro/Ejemplo1_12.py
@@ -0,0 +1,6 @@
+# El programa, dados los enteros A y B, calcula el resultado de una expresion
+A = int (input("Introduce tu primer numero entero"))
+B = int (input("Introduce otro numero entero"))
+RES = (A + B) ** 2/3
+print (f"Tu resultado es { RES }")
+
