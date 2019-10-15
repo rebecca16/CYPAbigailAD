beed64b79f684492b399ea410ca08f003a7b7ca6
rebecca16 <rebeccaaguilar688@gmai.com>
2019-10-15 17:39:04 -0500

    For, While

---
+++ libro/Ejemplo3_2.py
@@ -0,0 +1,6 @@
+Nomina = 0
+for i in range(1,11,1):
+    SUE = float (input("Ingresa el sueldo: "))
+    Nomina += SUE #Es equivalente a Nomina = Nomina + SUE
+print("La nomina de la empresa es: ", Nomina)    
+
